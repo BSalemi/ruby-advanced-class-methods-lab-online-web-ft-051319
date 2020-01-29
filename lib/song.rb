1ade5746c4dcae75a81cad5b176ea7904cccfa0d
Brian Salemi <briandsalemi@gmail.com>
2020-01-29 18:30:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,32 +13,23 @@ class Song
   end
 
   def self.create
-    song = self.new
+    song = Song.new
     song.save
-    @@all << song
     song
   end
 
-  def self.new_by_name(name)
+  def self.new_by_name(song_name)
     song = self.new
-    song.name = name
-    song.save
-    @@all << song
+    song.name = song_name
     song
   end
 
-  def self.create_by_name(name)
-    song = self.new
-    song.name = name
-    song.save
-    @@all << song
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
     song
   end
-
-  def self.find_by_name(name)
-    @@all.find{|song| song.name == name}
-  end
-
+  
   def self.find_or_create_by_name(song_name)
     self.find_by_name(song_name) || self.create_by_name(song_name)
   end
