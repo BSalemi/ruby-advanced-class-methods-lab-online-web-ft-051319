4aadcf3262ae100ad1999a58b2e991cb7d482509
Brian Salemi <briandsalemi@gmail.com>
2020-01-29 18:11:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,15 @@
 require 'pry'
 
 class Song
-  attr_accessor :name
-
+  attr_accessor :name, :artist_name
   @@all = []
-  def self.create(name)
-    song = self.new
-    song.name = name
-    @@all << song
+ 
+  def self.all
+    @@all
   end
+ 
+  def save
+    self.class.all << self
+  end
+ 
 end
